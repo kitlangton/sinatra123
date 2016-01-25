340179840427d130475560e4e5cd4944b1d5072f
Kit Langton <kitlangton@gmail.com>
2016-01-25 11:54:47 -0500

    Tests pass

--- Gemfile
+++ Gemfile
@@ -2,7 +2,10 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'rspec'
+gem 'thin'
+gem 'rack-flash3'
 gem 'guard-rspec'
 gem 'rerun'
 gem 'capybara'
--- app.rb
+++ app.rb
@@ -1,5 +1,42 @@
 require 'sinatra'
+require 'thin'
+require 'sinatra/reloader' if development?
+require 'rack-flash'
 require_relative 'lib/tower_of_hanoi'
 
+use Rack::Flash
 enable :sessions
 
+helpers do
+  def load_tower
+    unless session[:towers]
+      TowerOfHanoi.new
+    else
+      TowerOfHanoi.new(session[:towers])
+    end
+  end
+
+  def save_tower(tower_of_hanoi)
+    session[:towers] = tower_of_hanoi.towers
+  end
+end
+
+get '/' do
+  tower_of_hanoi = load_tower
+  if tower_of_hanoi.win?
+    session[:towers] = nil
+  end
+  erb :game, locals: { tower_of_hanoi: tower_of_hanoi }
+end
+
+post '/move' do
+  tower_of_hanoi = load_tower
+  from, to = params[:from].to_i, params[:to].to_i
+  if tower_of_hanoi.valid_move?(from,to)
+    tower_of_hanoi.move(from, to)
+    save_tower(tower_of_hanoi)
+  else
+    flash[:notice] = "Invalid move"
+  end
+  redirect '/'
+end
